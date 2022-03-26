a7d33b098737341c206e9c84fe09e2cba47a958c
Diego Kaku <diegokaku@gmail.com>
2022-03-25 22:26:45 -0500

    index model? need to test and category-routes, need test

--- models/index.js
+++ models/index.js
@@ -5,13 +5,43 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// Category hasMany Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Product hasMany Tags
+Product.hasMany(Tag, {
+  foreignKey: 'product_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tag hasMany Products
+Tag.hasMany(Product, {
+  foreignKey: 'tag_id',
+});
 
+// ProductTag belongsTo Product
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+});
+
+// Product belongsToMany Tags
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+// Tag belongsToMany Products
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+// export
 module.exports = {
   Product,
   Category,
